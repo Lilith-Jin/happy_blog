0c2fb9831b2265c34f380bd076c101c29b66ca10
Lilith <li6253462@gmail.com>
2022-03-19 15:04:50 +0800

    add association between user and blog

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,4 @@
 class Blog < ApplicationRecord
+  has_many :user_blogs, dependent: :delete_all
+  has_many :users, through: :user_blogs
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :user_blogs, dependent: :delete_all
+  has_many :blogs, through: :user_blogs
 end

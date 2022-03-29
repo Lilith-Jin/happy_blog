1b9ae166fc457d0468a3a154ec0d2e50d820f584
Lilith <li6253462@gmail.com>
2022-03-30 00:31:19 +0800

    design the routes for article

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :blogs do
+    resources :articles, shallow: true do
+      member do
+        get :public
+      end
+    end
     resources :user_blogs, only:[:index, :new, :create]
   end
     resources :user_blogs, only:[:show, :edit, :update, :destroy]

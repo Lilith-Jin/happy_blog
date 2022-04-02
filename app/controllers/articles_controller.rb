f0eef6b2e0daaa2a16a629ce50e819889b2962f5
Lilith <li6253462@gmail.com>
2022-04-02 23:40:30 +0800

    adjust link on index of article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :authenticate_user!
   before_action :find_blog, only:[:index, :new, :create]
+  before_action :find_article, except:[:index, :new, :create]
 
   def index
     @articles = @blog.articles
@@ -11,7 +12,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = @blog.articles.new(article_params)
+    @article = @blog.articles.build(article_params)
     if @article.save
       redirect_to blog_articles_path, notice: "文章新增成功" 
     else
@@ -26,9 +27,17 @@ class ArticlesController < ApplicationController
   end
 
   def update
+    if @article.update(article_params)
+    redirect_to blog_articles_path, notice:"文章更新成功"
+    else
+      render :edit
+    end
   end
 
-  def delete
+  def destroy
+    @article.destroy
+    redirect_to blog_articles_path, notice:"文章刪除成功"
+    
   end
 
   private
@@ -39,4 +48,9 @@ class ArticlesController < ApplicationController
   def find_blog
     @blog = current_user.blogs.find(params[:blog_id])
   end
+
+  def find_article
+    @article = Article.find(params[:id])
+  end 
+
 end
